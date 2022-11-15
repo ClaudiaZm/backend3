a5834bceca78c5ff86cf95e93fe8d425447bbaf0
Estudiante Bloque 09 Sala 105  B09S105est <B09S105est@AULASMED.UPB>
2022-11-15 18:34:56 -0500

    backend 3 renovado

---
+++ README.md
@@ -0,0 +1,7 @@
+Debes crear las variables de entorno,te dejo un archivo llamado .envTemplate como guia
+
+Para ejecutar el backend por favor seguir las siguientes instruciones
+
+1. Instalar las dependencias npm i
+2. Ejecutar el servidor  npm run dev 
+3. Disfrutar 
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import * as dotenv from 'dotenv' 
+dotenv.config()
+
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { connectDb } from './database.js';
+import { dirname } from "path";
+import { fileURLToPath } from 'url';
+
+// rutas
+import postRoutes from "./routes/postRoute.js"
+
+connectDb()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.set('Port', process.env.PORT);
+app.use("/public",express.static(__dirname + "/storage/imgs"))
+app.use(morgan('dev'));
+app.use(cors({ origin: '*' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/post", postRoutes);
+
+app.listen(app.get('Port'), () => { console.log('servidor escuchando por el puerto', app.get('Port')); });
